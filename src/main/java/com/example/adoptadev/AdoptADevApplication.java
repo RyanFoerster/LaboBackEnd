f8ddce7edc56a32ae6bbe57c731b4ffdc93a3290
Loïc Vanorshoven <loic.vanorshoven@hotmail.com>
2023-09-05 13:46:27 +0200

    initial commit

---
+++ src/main/java/com/example/adoptadev/AdoptADevApplication.java
@@ -0,0 +1,13 @@
+package com.example.adoptadev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoptADevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdoptADevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/adoptadev/AdoptADevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.adoptadev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoptADevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
