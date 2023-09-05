76f621c8fd8c30a2ff87f8be9d750032cf2cb7f8
RyanFoerster <73285979+RyanFoerster@users.noreply.github.com>
2023-09-05 18:18:25 +0200

    Ajout des TechnologyBackEnd + TechnologyFrontEnd + adresse dans le User + ajout de contraintes unique sur certains champs

---
+++ src/main/java/be/techifutur/labo/adoptadev/models/enums/TechnologyBackEnd.java
@@ -0,0 +1,18 @@
+package be.techifutur.labo.adoptadev.models.enums;
+
+public enum TechnologyBackEnd {
+    NODE,
+    PYTHON_DJANGO,
+    RUBY_ON_RAILS,
+    SPRING_BOOT,
+    NET_CORE,
+    PHP_LARAVEL,
+    ASP_NET,
+    EXPRESS,
+    KOA,
+    GO,
+    RUST,
+    HASKELL,
+    SCALA
+
+}
---
+++ src/main/java/be/techifutur/labo/adoptadev/models/enums/TechnologyFrontEnd.java
@@ -0,0 +1,13 @@
+package be.techifutur.labo.adoptadev.models.enums;
+
+public enum TechnologyFrontEnd {
+
+    REACT,
+    VUE,
+    ANGULAR,
+    SVELTE,
+    JQUERY,
+    EMBER,
+    BACKBONE,
+    PREACT
+}
