2c7975700e093080824465bd8848cfdab60050d7
loic vanorshoven <loic.vanorshoven@hotmail.com>
2023-09-05 23:41:32 +0200

    CrudService ready

--- src/main/java/be/techifutur/labo/adoptadev/services/UserService.java
+++ src/main/java/be/techifutur/labo/adoptadev/services/UserService.java
@@ -4,10 +4,11 @@ import be.techifutur.labo.adoptadev.models.entities.Dev;
 import be.techifutur.labo.adoptadev.models.entities.Recruiter;
 import be.techifutur.labo.adoptadev.models.entities.User;
 
+import java.util.List;
+
 public interface UserService {
 
     void devRegister(Dev dev);
     void recruiterRegister(Recruiter recruiter);
-
     String login(String username, String password);
 }
