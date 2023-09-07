bffed145404cac3ff00b119c1617535ebb2457d7
Loïc Vanorshoven <lovanorshoven@gmail.com>
2023-09-07 11:29:45 +0200

    update dev et recruiter corrigés

--- src/main/java/be/techifutur/labo/adoptadev/controllers/DevController.java
+++ src/main/java/be/techifutur/labo/adoptadev/controllers/DevController.java
@@ -4,6 +4,7 @@ import be.techifutur.labo.adoptadev.models.dtos.DevDTO;
 import be.techifutur.labo.adoptadev.models.forms.DevProfileUpdateForm;
 import be.techifutur.labo.adoptadev.repositories.UserRepository;
 import be.techifutur.labo.adoptadev.services.UserService;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,8 +18,8 @@ public class DevController {
         this.userService = userService;
     }
 
-    @PutMapping("/{id[0-9]+}")
-    public ResponseEntity<DevDTO> update(@PathVariable Long id, @RequestBody DevProfileUpdateForm form) {
+    @PutMapping("/{id:[0-9]+}")
+    public ResponseEntity<DevDTO> update(@PathVariable Long id, @RequestBody @Valid DevProfileUpdateForm form) {
         userService.updateDev(id, form.toEntity());
         return ResponseEntity.noContent()
                 .build();
--- src/main/java/be/techifutur/labo/adoptadev/models/forms/DevProfileUpdateForm.java
+++ src/main/java/be/techifutur/labo/adoptadev/models/forms/DevProfileUpdateForm.java
@@ -16,12 +16,6 @@ import java.util.Set;
 @Builder
 public class DevProfileUpdateForm {
 
-    @Size(min = 6)
-    @Pattern(
-            regexp = "^(?=.*[!=@#$%^&*()_+{}\\\\[\\\\]:;<>,.?~\\\\-]).*(?=.*[A-Z]).*(?=.*[0-9]).*$",
-            message = "Doit contenir une majuscule, un chiffre, un caractère spécial et minimum 6 caractères"
-    )
-    private String password;
     private String firstName;
     private String lastName;
     @Email
@@ -37,7 +31,7 @@ public class DevProfileUpdateForm {
 
     public Dev toEntity(){
         Dev dev = new Dev();
-        dev.setPassword(this.password);
+
         dev.setFirstName(this.firstName);
         dev.setLastName(this.lastName);
         dev.setEmail(this.email);
